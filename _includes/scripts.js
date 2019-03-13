7398c20fe23ff4b67275d61c6880cfb2e26ac8f8
weru <onewesh@gmail.com>
2019-03-13 20:54:30 +0300

    add comments

--- _includes/scripts.js
+++ _includes/scripts.js
@@ -1,61 +1,67 @@
+// select one element
 function elem(selector, parent = document){
     let elem = document.querySelector(selector);
     return elem != false ? elem : false;
-  }
-  
-  function elems(selector) {
+}
+
+// select multiple elements
+function elems(selector) {
     let elems = document.querySelectorAll(selector);
     return elems.length ? elems : false; 
-  }
-  
-  function pushClass(el, targetClass) {
+}
+
+// add class
+function pushClass(el, targetClass) {
     // equivalent to addClass
     if (el && typeof el == 'object' && targetClass) {
-      elClass = el.classList;
-      elClass.contains(targetClass) ? false : elClass.add(targetClass);
+        elClass = el.classList;
+        elClass.contains(targetClass) ? false : elClass.add(targetClass);
     }
-  }
-  
-  function deleteClass(el, targetClass) {
+}
+
+//delete class
+function deleteClass(el, targetClass) {
     // equivalent to removeClass
     if (el && typeof el == 'object' && targetClass) {
-      elClass = el.classList;
-      elClass.contains(targetClass) ? elClass.remove(targetClass) : false;
+        elClass = el.classList;
+        elClass.contains(targetClass) ? elClass.remove(targetClass) : false;
     }
-  }
-  
-  function modifyClass(el, targetClass) {
+}
+
+// toggle class
+function modifyClass(el, targetClass) {
     // equivalent to toggleClass
     if (el && typeof el == 'object' && targetClass) {
-      elClass = el.classList;
-      elClass.contains(targetClass) ? elClass.remove(targetClass) : elClass.add(targetClass);
+        elClass = el.classList;
+        elClass.contains(targetClass) ? elClass.remove(targetClass) : elClass.add(targetClass);
     }
-  }
-  
-  function containsClass(el, targetClass) {
+}
+
+// check if element has a class
+function containsClass(el, targetClass) {
     if (el && typeof el == 'object' && targetClass) {
-      return el.classList.contains(targetClass) ? true : false;
+        return el.classList.contains(targetClass) ? true : false;
     }
-  }
+}
 
 (function() {
-  let times = elems('.session_start');
-  if(times) {
-      times.forEach(function(time){
-        let t = time.innerHTML.trim();
-        let hours = t.substr(0, 2);
-        let minutes = t.substr(2, 4)
-        let hoursEl = document.createElement('span');
-        hoursEl.classList.add('session_hour');
-        hoursEl.innerHTML = hours;
-        let minutesEl = document.createElement('span');
-        minutesEl.classList.add('session_minutes');
-        minutesEl.innerHTML = minutes;
-        time.innerHTML = '';
-        time.appendChild(hoursEl);
-        time.appendChild(minutesEl);
-      });
-  }
+    let times = elems('.session_start');
+    if(times) {
+        times.forEach(function(time){
+            let t = time.innerHTML.trim();
+            let hours = t.substr(0, 2);
+            let minutes = t.substr(2, 4)
+            let hoursEl = document.createElement('span');
+            hoursEl.classList.add('session_hour');
+            hoursEl.innerHTML = hours;
+            let minutesEl = document.createElement('span');
+            minutesEl.classList.add('session_minutes');
+            minutesEl.innerHTML = minutes;
+            time.innerHTML = '';
+            time.appendChild(hoursEl);
+            time.appendChild(minutesEl);
+        });
+    }
 })();
 
 var navLinksDiv = document.getElementById("nav-links-div");
